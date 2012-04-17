ce90abcf48c6774d32065050679077c015d3eaca
Christian Heinrich <christian.heinrich@cmlh.id.au>
2012-04-17 18:01:35 +1000

    RESOLVED ./Local_Transforms/etc

--- Build/create_release.sh
+++ Build/create_release.sh
@@ -5,7 +5,7 @@
 # christian.heinrich@cmlh.id.au
 #
 cd ..
-tar -cvf ./Releases/$1.tar ./Local_Transforms/*.pl
-tar -rvf ./Releases/$1.tar ./Entities/*.mtz
-gzip ./Releases/$1.tar
+tar -cvf ./Releases/Maltego-Facebook-$1.tar ./Local_Transforms/etc/*.conf ./Local_Transforms/*.pl
+tar -rvf ./Releases/Maltego-Facebook-$1.tar ./Entities/*.mtz
+gzip ./Releases/Maltego-Facebook-$1.tar
 cd Build
