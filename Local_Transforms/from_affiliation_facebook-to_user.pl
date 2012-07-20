beb7338c5c58ef512d81e8027e6765b51c937181
Christian Heinrich <christian.heinrich@cmlh.id.au>
2012-07-20 18:10:19 +1000

    Refactored ./Local_Transforms/from_affiliation_facebook-to_user.pl file

--- Local_Transforms/from_affiliation_facebook-to_user.pl
+++ Local_Transforms/from_affiliation_facebook-to_user.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/env perl
+# The above shebang is for "perlbrew", otherwise use /usr/bin/perl or the file path quoted for "which perl"
 #
 # Please refer to the Plain Old Documentation (POD) at the end of this Perl Script for further information
 
