b6c6cbc3cc7f8ce1928a8c1b143839727df7a6ad
Christian Heinrich <christian.heinrich@cmlh.id.au>
2012-07-20 19:39:08 +1000

    Refactored ./Local_Transforms/t/*.pl

--- Local_Transforms/t/https_graphapi_url.pl
+++ Local_Transforms/t/https_graphapi_url.pl
@@ -4,6 +4,7 @@
 # Please refer to the Plain Old Documentation (POD) at the end of this Perl Script for further information
 
 use strict;
+
 # Not possible to define number of tests as it is defined by the number of *.pl files in ../Local_Transforms
 use Test::More "no_plan";
 
@@ -19,15 +20,15 @@ my @pl_files;
 my $pl_file;
 
 foreach $pl_file (@pl_files) {
-	print $pl_file . "\n";
-	open (PLFILE, $pl_file) or die "Could not open $pl_file";
-	my $line;
-	foreach $line (<PLFILE>) {
-		if ($line =~ "://graph.facebook.com/") {
-			ok($line =~ "https://graph.facebook.com/", "GraphAPI https URL");
-			diag("$line");
-		}
-	}
+    print $pl_file . "\n";
+    open( PLFILE, $pl_file ) or die "Could not open $pl_file";
+    my $line;
+    foreach $line (<PLFILE>) {
+        if ( $line =~ "://graph.facebook.com/" ) {
+            ok( $line =~ "https://graph.facebook.com/", "GraphAPI https URL" );
+            diag("$line");
+        }
+    }
 }
 
 =head1 NAME
