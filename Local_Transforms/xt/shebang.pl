4b8ddfea6a3bd730317f6e88a26770091246b8bf
Christian Heinrich <christian.heinrich@cmlh.id.au>
2012-07-20 18:15:10 +1000

    Added ./Local_Transforms/xt/shebang.pl file

---
+++ Local_Transforms/xt/shebang.pl
@@ -0,0 +1,114 @@
+#!/usr/bin/env perl
+# The above shebang is for "perlbrew", otherwise use /usr/bin/perl or the file path quoted for "which perl"
+#
+# Please refer to the Plain Old Documentation (POD) at the end of this Perl Script for further information
+
+use strict;
+# Not possible to define number of tests as it is defined by the number of *.pl files in ../Local_Transforms
+use Test::More "no_plan";
+
+# #CONFIGURATION Remove "#" for Smart::Comments
+# use Smart::Comments;
+
+my $VERSION = "0.0.1"; # May be required to upload script to CPAN i.e. http://www.cpan.org/scripts/submitting.html
+
+my @pl_files;
+
+@pl_files = <../*.pl>;
+
+my $pl_file;
+
+foreach $pl_file (@pl_files) {
+	# TODO Exclude to_bookmarked.pl from this test as sub split_maltego_additional_fields is not required by it.
+	print $pl_file . "\n";
+	open (PLFILE, $pl_file) or die "Could not open $pl_file";
+	my $line;
+	foreach $line (<PLFILE>) {
+		if ($line =~ "# The above shebang is for") {
+			ok($line =~ "shebang", "shebang");
+			diag("$line");
+		}
+	}
+}
+
+=head1 NAME
+
+split_maltego_additional_fields.pl - "sub split_maltego_additional_fields"
+
+=head1 VERSION
+
+This documentation refers "sub split_maltego_additional_fields" Alpha v$VERSION
+
+=head1 CONFIGURATION
+
+Set the value(s) marked as #CONFIGURATION above this POD
+    
+=head1 USAGE
+
+split_maltego_additional_fields.pl
+
+=head1 REQUIRED ARGUEMENTS
+                
+=head1 OPTIONAL ARGUEMENTS
+
+=head1 DESCRIPTION
+
+Test to ensure that sub split_maltego_additional_fields is refactored.
+
+=head1 DEPENDENCIES
+
+=head1 PREREQUISITES
+
+=head1 COREQUISITES
+
+=head1 INSTALLATION
+
+=head1 OSNAMES
+
+osx
+
+=head1 SCRIPT CATEGORIES
+
+Web
+
+=head1 INCOMPATIBILITIES
+
+=head1 BUGS AND LIMITATIONS
+
+Please refer to the comments beginning with "TODO" in the Perl Code.
+
+=head1 AUTHOR
+
+Christian Heinrich
+
+=head1 CONTACT INFORMATION
+
+http://cmlh.id.au/contact
+
+=head1 MAILING LIST
+
+=head1 REPOSITORY
+
+http://github.com/cmlh/Maltego-Facebook
+
+=head1 FURTHER INFORMATION AND UPDATES
+
+http://cmlh.id.au/tagged/maltego
+
+=head1 LICENSE AND COPYRIGHT
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. 
+
+Copyright 2012 Christian Heinrich
+
+
