ce3eea639f30de473eb8bbb8fe47c4fd0a5eb517
Christian Heinrich <christian.heinrich@cmlh.id.au>
2012-07-24 18:58:16 +1000

    Refactored ./Build/create_release.sh file

--- Build/create_release.sh
+++ Build/create_release.sh
@@ -5,6 +5,9 @@
 # christian.heinrich@cmlh.id.au
 #
 cd ..
+rm -rf ./Local_Transforms/Images/*
+mkdir ./Local_Transforms/Images/Pictures
+mkdir ./Local_Transforms/Images/Covers
 tar -cvf ./Releases/Maltego-Facebook-$1.tar ./Local_Transforms/etc/*.conf ./Local_Transforms/*.pl ./Local_Transforms/Images/*
 tar -rvf ./Releases/Maltego-Facebook-$1.tar ./Entities/*.mtz
 gzip ./Releases/Maltego-Facebook-$1.tar
