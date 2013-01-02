bf57550e6069330a9a71d606cec82691fd11811f
Christian Heinrich <christian.heinrich@cmlh.id.au>
2013-01-02 17:40:26 +1100

    Add t/maltego_cmd-line.sh

---
+++ Local_Transforms/t/maltego_cmd-line.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# This is a workaround until I convert this to TAP
+#
+# christian.heinrich@cmlh.id.au
+# 
+cd ..
+./from_affiliation_facebook-to_user_cover.pl "Christian Heinrich" "person.name=Christian Heinrich#network=Facebook#uid=100002552265389#profile_url=http://www.facebook.com/cmlh.id.au"
+
