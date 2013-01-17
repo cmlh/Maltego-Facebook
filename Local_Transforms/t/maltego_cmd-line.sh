0d6cd100ac0f4ac6b781d71b9fff772b08b3862f
Christian Heinrich <christian.heinrich@cmlh.id.au>
2013-01-17 14:06:40 +1100

    Feature "Dhillon Kannabhiran" Test Data

--- Local_Transforms/t/maltego_cmd-line.sh
+++ Local_Transforms/t/maltego_cmd-line.sh
@@ -5,5 +5,7 @@
 # christian.heinrich@cmlh.id.au
 # 
 cd ..
+# No Cover Image
 ./from_affiliation_facebook-to_user_cover.pl "Christian Heinrich" "person.name=Christian Heinrich#network=Facebook#uid=100002552265389#profile_url=http://www.facebook.com/cmlh.id.au"
-
+# With Cover Image
+./from_affiliation_facebook-to_user_cover.pl "Dhillon Kannabhiran" "person.name=Dhillon Kannabhiran#network=facebook#uid=547636255#profile_url=http://www.facebook.com/profile.php?id\\=547636255#icon-url=http://profile.ak.fbcdn.net/hprofile-ak-ash4/260979_547636255_1707259828_n.jpg"
