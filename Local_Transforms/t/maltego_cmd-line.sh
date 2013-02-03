3bc3924d76aa6de06944f44873f3dd6e622bb780
Christian Heinrich <christian.heinrich@cmlh.id.au>
2013-02-03 13:39:49 +1100

    Feature two more tests

--- Local_Transforms/t/maltego_cmd-line.sh
+++ Local_Transforms/t/maltego_cmd-line.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 #
-# This is a workaround until I convert this to TAP
+# This is a workaround until I convert this to TAP i.e. http://search.cpan.org/~bdfoy/Test-Output-1.01/lib/Test/Output.pm
 #
 # christian.heinrich@cmlh.id.au
 # 
 cd ..
 # No Cover Image
 ./from_affiliation_facebook-to_user_cover.pl "Christian Heinrich" "person.name=Christian Heinrich#network=Facebook#uid=100002552265389#profile_url=http://www.facebook.com/cmlh.id.au"
-# With Cover Image
+# With Cover Image but not stored in ./Images/Covers 
+rm ./Images/Covers/DhillonKannabhiran.jpg
+./from_affiliation_facebook-to_user_cover.pl "Dhillon Kannabhiran" "person.name=Dhillon Kannabhiran#network=facebook#uid=547636255#profile_url=http://www.facebook.com/profile.php?id\\=547636255#icon-url=http://profile.ak.fbcdn.net/hprofile-ak-ash4/260979_547636255_1707259828_n.jpg"
+# With Cover Image stored in ./Images/Covers
 ./from_affiliation_facebook-to_user_cover.pl "Dhillon Kannabhiran" "person.name=Dhillon Kannabhiran#network=facebook#uid=547636255#profile_url=http://www.facebook.com/profile.php?id\\=547636255#icon-url=http://profile.ak.fbcdn.net/hprofile-ak-ash4/260979_547636255_1707259828_n.jpg"
