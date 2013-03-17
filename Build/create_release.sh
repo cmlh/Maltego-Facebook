29be6de06f3d40c5231936d93e18c1f1f733d205
Christian Heinrich <christian.heinrich@cmlh.id.au>
2013-03-18 06:56:35 +1100

    Feature "git describe"

--- Build/create_release.sh
+++ Build/create_release.sh
@@ -4,8 +4,10 @@
 #
 # christian.heinrich@cmlh.id.au
 #
+git describe --all --abbrev=4 HEAD^ > version.txt
 cd ..
-tar -cvf ./Releases/Maltego-Facebook-$1.tar ./Local_Transforms/etc/*.conf ./Local_Transforms/*.pl ./Local_Transforms/Images/*
-tar -rvf ./Releases/Maltego-Facebook-$1.tar ./Entities/*.mtz
-gzip ./Releases/Maltego-Facebook-$1.tar
+tar -cvf ./Releases/Maltego-Facebook-`git describe`.tar ./Local_Transforms/etc/*.conf ./Local_Transforms/*.pl ./Local_Transforms/Images/*
+tar -rvf ./Releases/Maltego-Facebook-`git describe`.tar ./Entities/*.mtz
+tar -rvf ./Releases/Maltego-Facebook-`git describe`.tar ./Build/version.txt
+gzip ./Releases/Maltego-Facebook-`git describe`.tar
 cd Build
