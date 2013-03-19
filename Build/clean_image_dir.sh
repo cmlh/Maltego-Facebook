591eb3fb66092996f5c2860a9b6920d3573c44d8
Christian Heinrich <christian.heinrich@cmlh.id.au>
2013-03-20 07:04:02 +1100

    Feature clean_image_dir and clean_local_transform files

---
+++ Build/clean_image_dir.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Creates the download release
+#
+# christian.heinrich@cmlh.id.au
+#
+cd ../Local_Transforms/Images/
+rm -v ./Covers/*
+rm -v ./Pictures/*
+cd ../../Build
---
+++ Build/clean_local_transforms.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Creates the download release
+#
+# christian.heinrich@cmlh.id.au
+#
+cd ../Local_Transforms/
+rm -v *.tdy
+cd ../Build
