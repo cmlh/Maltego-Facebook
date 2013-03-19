db6034c0d2059f77696562422894441cf07a8556
Christian Heinrich <christian.heinrich@cmlh.id.au>
2013-03-19 17:24:07 +1100

    Refactor version.txt->git_blob.txt

--- Build/create_release.sh
+++ Build/create_release.sh
@@ -4,10 +4,10 @@
 #
 # christian.heinrich@cmlh.id.au
 #
-git describe --all --abbrev=4 HEAD^ > version.txt
+git describe --long --abbrev=4 > git_blob.txt
 cd ..
 tar -cvf ./Releases/Maltego-Facebook-`git describe`.tar ./Local_Transforms/etc/*.conf ./Local_Transforms/*.pl ./Local_Transforms/Images/*
 tar -rvf ./Releases/Maltego-Facebook-`git describe`.tar ./Entities/*.mtz
-tar -rvf ./Releases/Maltego-Facebook-`git describe`.tar ./Build/version.txt
+tar -rvf ./Releases/Maltego-Facebook-`git describe`.tar ./Build/git_blob.txt
 gzip ./Releases/Maltego-Facebook-`git describe`.tar
 cd Build
